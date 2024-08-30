d353336ed240d50b6df222af64c309b56a711e60
alihakankurt <kurtalihakan@outlook.com>
2024-08-30 23:02:33 +0300

    Add `Core` layer

---
+++ Ryn.Core/Include/Ryn/Core/Types.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+namespace Ryn
+{
+    using i8 = uint8_t;
+    using i16 = uint16_t;
+    using i32 = uint32_t;
+    using i64 = uint64_t;
+    using isz = ptrdiff_t;
+
+    using u8 = int8_t;
+    using u16 = int16_t;
+    using u32 = int32_t;
+    using u64 = int64_t;
+    using usz = size_t;
+
+    using f32 = float;
+    using f64 = double;
+}
