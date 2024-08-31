1d9079f74ded58a2c942b80f4acafbb5d31bf570
alihakankurt <kurtalihakan@outlook.com>
2024-08-31 13:38:22 +0300

    Fix signs of integer definitions in `Core` layer

--- Ryn.Core/Include/Ryn/Core/Types.hpp
+++ Ryn.Core/Include/Ryn/Core/Types.hpp
@@ -5,16 +5,16 @@
 
 namespace Ryn::Core
 {
-    using i8 = uint8_t;
-    using i16 = uint16_t;
-    using i32 = uint32_t;
-    using i64 = uint64_t;
+    using i8 = int8_t;
+    using i16 = int16_t;
+    using i32 = int32_t;
+    using i64 = int64_t;
     using isz = ptrdiff_t;
 
-    using u8 = int8_t;
-    using u16 = int16_t;
-    using u32 = int32_t;
-    using u64 = int64_t;
+    using u8 = uint8_t;
+    using u16 = uint16_t;
+    using u32 = uint32_t;
+    using u64 = uint64_t;
     using usz = size_t;
 
     using f32 = float;
