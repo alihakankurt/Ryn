92a82f0d0ff5631805238d9ddf21af59cc7d3ac0
alihakankurt <kurtalihakan@outlook.com>
2024-09-03 13:20:53 +0300

    Fix compare and copy functions under the namespace `Memory`

--- Ryn.Core/Include/Ryn/Core/Memory.hpp
+++ Ryn.Core/Include/Ryn/Core/Memory.hpp
@@ -2,89 +2,93 @@
 
 #include <Ryn/Core/Types.hpp>
 
-namespace Ryn::Core
+namespace Ryn::Core::Memory
 {
-    class Memory
+    template <typename TValue>
+    static constexpr isz Compare(const TValue* source1, const TValue* source2, usz count)
     {
-      public:
-        template <typename TValue>
-        static constexpr isz Compare(const TValue* source1, const TValue* source2, usz count)
+        if (count == 0 || source1 == source2)
+            return 0;
+
+        usz size = count * sizeof(TValue);
+        const u8* src1 = reinterpret_cast<const u8*>(source1);
+        const u8* src2 = reinterpret_cast<const u8*>(source2);
+
+        while (size >= sizeof(isz))
         {
-            if (count == 0 || source1 == source2)
-                return 0;
-
-            usz size = count * sizeof(TValue);
-            while (size >= sizeof(isz))
+            if (isz difference = *reinterpret_cast<const isz*>(src1) - *reinterpret_cast<const isz*>(src2); difference != 0)
             {
-                isz difference = *reinterpret_cast<const isz*>(source1) - *reinterpret_cast<const isz*>(source2);
-                if (difference != 0)
-                    return difference;
+                return difference;
+            }
 
-                source1 += sizeof(isz);
-                source2 += sizeof(isz);
-                size -= sizeof(isz);
+            src1 += sizeof(isz);
+            src2 += sizeof(isz);
+            size -= sizeof(isz);
+        }
+
+        while (size > 0)
+        {
+            if (isz difference = *src1 - *src2; difference != 0)
+            {
+                return difference;
+            }
+
+            src1 += sizeof(u8);
+            src2 += sizeof(u8);
+            size -= sizeof(u8);
+        }
+
+        return 0;
+    }
+
+    template <typename TValue>
+    static constexpr void Copy(TValue* destination, const TValue* source, usz count)
+    {
+        if (count == 0 || destination == source)
+            return;
+
+        usz size = count * sizeof(TValue);
+        u8* dst = reinterpret_cast<u8*>(destination);
+        const u8* src = reinterpret_cast<const u8*>(source);
+
+        if (destination < source)
+        {
+            while (size >= sizeof(usz))
+            {
+                *reinterpret_cast<usz*>(dst) = *reinterpret_cast<const usz*>(src);
+                dst += sizeof(usz);
+                src += sizeof(usz);
+                size -= sizeof(usz);
             }
 
             while (size > 0)
             {
-                isz difference = *reinterpret_cast<const i8*>(source1) - *reinterpret_cast<const i8*>(source2);
-                if (difference != 0)
-                    return difference;
-
-                source1 += sizeof(i8);
-                source2 += sizeof(i8);
-                size -= sizeof(i8);
+                *dst = *src;
+                dst += sizeof(u8);
+                src += sizeof(u8);
+                size -= sizeof(u8);
             }
-
-            return 0;
         }
-
-        template <typename TValue>
-        static constexpr void Copy(TValue* destination, const TValue* source, usz count)
+        else
         {
-            if (count == 0 || destination == source)
-                return;
+            dst += size;
+            src += size;
 
-            usz size = count * sizeof(TValue);
-            if (destination < source)
+            while (size >= sizeof(usz))
             {
-                while (size >= sizeof(usz))
-                {
-                    *reinterpret_cast<usz*>(destination) = *reinterpret_cast<const usz*>(source);
-                    destination += sizeof(usz);
-                    source += sizeof(usz);
-                    size -= sizeof(usz);
-                }
-
-                while (size > 0)
-                {
-                    *reinterpret_cast<u8*>(destination) = *reinterpret_cast<const u8*>(source);
-                    destination += sizeof(u8);
-                    source += sizeof(u8);
-                    size -= sizeof(u8);
-                }
+                dst -= sizeof(usz);
+                src -= sizeof(usz);
+                *reinterpret_cast<usz*>(dst) = *reinterpret_cast<const usz*>(src);
+                size -= sizeof(usz);
             }
-            else
+
+            while (size > 0)
             {
-                destination += size;
-                source += size;
-
-                while (size >= sizeof(usz))
-                {
-                    destination -= sizeof(usz);
-                    source -= sizeof(usz);
-                    *reinterpret_cast<usz*>(destination) = *reinterpret_cast<const usz*>(source);
-                    size -= sizeof(usz);
-                }
-
-                while (size > 0)
-                {
-                    destination -= sizeof(u8);
-                    source -= sizeof(u8);
-                    *reinterpret_cast<u8*>(destination) = *reinterpret_cast<const u8*>(source);
-                    size -= sizeof(u8);
-                }
+                dst -= sizeof(u8);
+                src -= sizeof(u8);
+                *dst = *src;
+                size -= sizeof(u8);
             }
         }
-    };
+    }
 }
