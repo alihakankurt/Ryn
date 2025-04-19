785616f6f842ea106df5e9e1b4344bed3c68ce87
alihakankurt <kurtalihakan@outlook.com>
2025-04-19 13:45:30 +0300

    Refactor Iterator/Iterable types slightly

--- Ryn.Core/Include/Ryn/Core/Iterable.hpp
+++ Ryn.Core/Include/Ryn/Core/Iterable.hpp
@@ -22,26 +22,26 @@ namespace Ryn
         constexpr Iterator(TValue* data) :
             Data(data) {}
 
-        virtual constexpr Iterator& operator++() = 0;
-        virtual constexpr Iterator& operator--() = 0;
+        virtual Iterator& operator++() = 0;
+        virtual Iterator& operator--() = 0;
 
-        virtual constexpr Iterator& operator+=(usz offset) = 0;
-        virtual constexpr Iterator& operator-=(usz offset) = 0;
+        virtual Iterator& operator+=(usz offset) = 0;
+        virtual Iterator& operator-=(usz offset) = 0;
 
-        constexpr TValue& operator*() { return *Data; }
-        constexpr const TValue& operator*() const { return *Data; }
+        TValue& operator*() { return *Data; }
+        const TValue& operator*() const { return *Data; }
 
-        constexpr TValue* operator->() { return Data; }
-        constexpr const TValue* operator->() const { return Data; }
+        TValue* operator->() { return Data; }
+        const TValue* operator->() const { return Data; }
 
-        constexpr bool operator!() const { return !Data; }
-        constexpr operator bool() const { return Data; }
+        bool operator!() const { return !Data; }
+        operator bool() const { return Data; }
 
-        virtual constexpr bool operator==(const Iterator<Traits::RemoveConst<TValue>>& other) const { return Data == other.Data; }
-        virtual constexpr bool operator!=(const Iterator<Traits::RemoveConst<TValue>>& other) const { return Data != other.Data; }
+        virtual bool operator==(const Iterator<Traits::RemoveConst<TValue>>& other) const { return Data == other.Data; }
+        virtual bool operator!=(const Iterator<Traits::RemoveConst<TValue>>& other) const { return Data != other.Data; }
 
-        virtual constexpr bool operator==(const Iterator<const Traits::RemoveConst<TValue>>& other) const { return Data == other.Data; }
-        virtual constexpr bool operator!=(const Iterator<const Traits::RemoveConst<TValue>>& other) const { return Data != other.Data; }
+        virtual bool operator==(const Iterator<const Traits::RemoveConst<TValue>>& other) const { return Data == other.Data; }
+        virtual bool operator!=(const Iterator<const Traits::RemoveConst<TValue>>& other) const { return Data != other.Data; }
     };
 
     template <typename TValue>
@@ -54,39 +54,39 @@ namespace Ryn
         constexpr LinearIterator(TValue* data) :
             Iterator<TValue>{data} {}
 
-        constexpr LinearIterator& operator++() override
+        LinearIterator& operator++() override
         {
             ++this->Data;
             return *this;
         }
 
-        constexpr LinearIterator& operator--() override
+        LinearIterator& operator--() override
         {
             --this->Data;
             return *this;
         }
 
-        constexpr LinearIterator& operator+=(usz offset) override
+        LinearIterator& operator+=(usz offset) override
         {
             this->Data += offset;
             return *this;
         }
 
-        constexpr LinearIterator& operator-=(usz offset) override
+        LinearIterator& operator-=(usz offset) override
         {
             this->Data -= offset;
             return *this;
         }
 
-        constexpr bool operator<(const LinearIterator<Traits::RemoveConst<TValue>>& other) const { return this->Data < other.Data; }
-        constexpr bool operator>(const LinearIterator<Traits::RemoveConst<TValue>>& other) const { return this->Data > other.Data; }
-        constexpr bool operator<=(const LinearIterator<Traits::RemoveConst<TValue>>& other) const { return this->Data <= other.Data; }
-        constexpr bool operator>=(const LinearIterator<Traits::RemoveConst<TValue>>& other) const { return this->Data >= other.Data; }
+        bool operator<(const LinearIterator<Traits::RemoveConst<TValue>>& other) const { return this->Data < other.Data; }
+        bool operator>(const LinearIterator<Traits::RemoveConst<TValue>>& other) const { return this->Data > other.Data; }
+        bool operator<=(const LinearIterator<Traits::RemoveConst<TValue>>& other) const { return this->Data <= other.Data; }
+        bool operator>=(const LinearIterator<Traits::RemoveConst<TValue>>& other) const { return this->Data >= other.Data; }
 
-        constexpr bool operator<(const LinearIterator<const Traits::RemoveConst<TValue>>& other) const { return this->Data < other.Data; }
-        constexpr bool operator>(const LinearIterator<const Traits::RemoveConst<TValue>>& other) const { return this->Data > other.Data; }
-        constexpr bool operator<=(const LinearIterator<const Traits::RemoveConst<TValue>>& other) const { return this->Data <= other.Data; }
-        constexpr bool operator>=(const LinearIterator<const Traits::RemoveConst<TValue>>& other) const { return this->Data >= other.Data; }
+        bool operator<(const LinearIterator<const Traits::RemoveConst<TValue>>& other) const { return this->Data < other.Data; }
+        bool operator>(const LinearIterator<const Traits::RemoveConst<TValue>>& other) const { return this->Data > other.Data; }
+        bool operator<=(const LinearIterator<const Traits::RemoveConst<TValue>>& other) const { return this->Data <= other.Data; }
+        bool operator>=(const LinearIterator<const Traits::RemoveConst<TValue>>& other) const { return this->Data >= other.Data; }
     };
 
     template <typename TValue, typename TIterator = Iterator<TValue>, typename TConstIterator = Iterator<const TValue>>
@@ -99,29 +99,38 @@ namespace Ryn
         virtual TIterator end() = 0;
         virtual TConstIterator end() const = 0;
 
-        constexpr TIterator Begin() { return begin(); }
-        constexpr TConstIterator Begin() const { return begin(); }
+        TIterator Begin() { return begin(); }
+        TConstIterator Begin() const { return begin(); }
 
-        constexpr TIterator End() { return end(); }
-        constexpr TConstIterator End() const { return end(); }
+        TIterator End() { return end(); }
+        TConstIterator End() const { return end(); }
 
       public:
-        constexpr bool IsEmpty() const { return begin() == end(); }
+        bool IsEmpty() const { return begin() == end(); }
 
-        constexpr TValue* Data() { return &*begin(); }
-        constexpr const TValue* Data() const { return &*begin(); }
+        TValue* Data()
+        {
+            static_assert(Traits::Same<TIterator, LinearIterator<TValue>>, "Data() can only be called on linear iterators");
+            return &*begin();
+        }
 
-        constexpr TValue& First() { return *begin(); }
-        constexpr const TValue& First() const { return *begin(); }
+        const TValue* Data() const
+        {
+            static_assert(Traits::Same<TConstIterator, LinearIterator<const TValue>>, "Data() can only be called on linear iterators");
+            return &*begin();
+        }
 
-        constexpr TValue& Last() { return *(end() -= 1); }
-        constexpr const TValue& Last() const { return *(end() -= 1); }
+        TValue& First() { return *begin(); }
+        const TValue& First() const { return *begin(); }
 
-        constexpr TValue& At(usz index) { return *(begin() += index); }
-        constexpr const TValue& At(usz index) const { return *(begin() += index); }
+        TValue& Last() { return *(end() -= 1); }
+        const TValue& Last() const { return *(end() -= 1); }
 
-        constexpr TValue& operator[](usz index) { return At(index); }
-        constexpr const TValue& operator[](usz index) const { return At(index); }
+        TValue& At(usz index) { return *(begin() += index); }
+        const TValue& At(usz index) const { return *(begin() += index); }
+
+        TValue& operator[](usz index) { return At(index); }
+        const TValue& operator[](usz index) const { return At(index); }
 
         TConstIterator FindFirst(Predicate<TValue> predicate) const
         {
@@ -130,7 +139,6 @@ namespace Ryn
             {
                 if (predicate(*it))
                     return it;
-
                 ++it;
             }
 
@@ -157,7 +165,6 @@ namespace Ryn
             {
                 if (predicate(*it))
                     return true;
-
                 ++it;
             }
 
@@ -171,7 +178,6 @@ namespace Ryn
             {
                 if (!predicate(*it))
                     return false;
-
                 ++it;
             }
 
