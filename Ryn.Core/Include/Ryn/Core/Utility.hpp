92c0e299270afe2a23d58e828ae966c14514040e
alihakankurt <kurtalihakan@outlook.com>
2025-04-21 01:31:08 +0300

    Small fix on `Utility::Exchange` function

--- Ryn.Core/Include/Ryn/Core/Utility.hpp
+++ Ryn.Core/Include/Ryn/Core/Utility.hpp
@@ -24,20 +24,20 @@ namespace Ryn
             return static_cast<Traits::RemoveReference<TValue>&&>(value);
         }
 
-        template <typename TValue>
-        static constexpr TValue Exchange(TValue& value, TValue&& newValue = {})
+        template <typename TValue, typename TNewValue = TValue>
+        static constexpr TValue Exchange(TValue& value, TNewValue&& newValue = {})
         {
-            TValue oldValue = Move(value);
-            value = Forward<TValue>(newValue);
+            TValue oldValue = Utility::Move(value);
+            value = Utility::Forward<TValue>(newValue);
             return oldValue;
         }
 
         template <typename TValue>
         static constexpr void Swap(TValue& value1, TValue& value2)
         {
-            TValue temp = Move(value1);
-            value1 = Move(value2);
-            value2 = Move(temp);
+            TValue temp = Utility::Move(value1);
+            value1 = Utility::Move(value2);
+            value2 = Utility::Move(temp);
         }
     }
 }
